394b3948df2428ac2af01ae10fd904fc488c26e7
koichi-inoue <inoue.ko@gmail.com>
2021-05-27 15:24:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Gloval Value
+let img;
+
+// modelLoaded > Ready to Accept
+window.onload = function() {
+  img = document.getElementById("image");
+  img.addEventListener("dragover",function(ev){ ev.preventDefault();}, false);
+  img.addEventListener("drop", function(ev){ ev.preventDefault(); GetImage(ev);}, false);
+}
+
+// GetImage
+function GetImage(ev){
+
+  let file = ev.dataTransfer.files[0];
+
+  //let fileType = file.name.slice(-4).toLowerCase();
+  if( file.type !== 'image/jpeg' ) {
+    alert("File available only .jpg");
+    return;
+  }
+
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+
+  reader.onloadend = function() {
+
+    img.setAttribute("src",reader.result);
+
+    let txt="<h2>Image Information</h2>";
+    txt += "FileName：" + file.name +'<br>';
+    txt += "FileSize：" + file.size +' Byte<br>';
+    txt += '<br>';
+    document.getElementById('information').innerHTML = txt;
+
+  }
+
+}
